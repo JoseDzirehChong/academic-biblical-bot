4a1a211d3fd8b92c42907923a822ffd22c6ca69a
Jose Dzireh Chong <josedzirehchong@gmail.com>
2017-08-26 13:36:02 +1000

    magic numbers and paths galore, now no longer galore

--- academic-biblical-bot/AcademicBiblicalBot.py
+++ academic-biblical-bot/AcademicBiblicalBot.py
@@ -16,13 +16,14 @@ import bs4
 import os
 
 assert os.path.exists('praw.ini')
+assert os.path.exists('PastComments.txt')
 
 #ID of already answered comments goes here
 alreadyRespondedComments = 'PastComments.txt'
-
 automatedResponse = """This is an automatic notification that your comment has been removed. Your comment was a top level comment, and unless you were hostile it is likely that your comment was removed for not meeting /r/AcademicBiblical's standards. Please check that you have provided academic sources, which are almost always necessary. Once you have edited your comment to comply with the rules, please contact a moderator to get your comment reapproved.
 
 *I am a bot, bleep bloop. Contact my creator /u/JoseDzirehChong if there are any issues with this bot*. [Source code](https://github.com/JoseDzirehChong/academic-biblical-bot)"""
+comment_batch_size = 250
 
 def authenticate():
     
@@ -47,12 +48,12 @@ def run_bot(reddit):
     
     print("Getting 250 comments...\n")
     
-    for comment in reddit.subreddit("ABBotTestSite").comments(limit = 250):
+    for comment in reddit.subreddit("ABBotTestSite").comments(limit = comment_batch_size):
 
         if comment.parent_id == comment.link_id and find_duplicate_comments(comment.id) == False:
             print(comment.body)
             
-            with open("PastComments.txt", "a") as myfile:
+            with open(alreadyRespondedComments, "a") as myfile:
                 myfile.write(comment.id + "\n")
             
             
