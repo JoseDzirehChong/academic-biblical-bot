44286ee719a3a517866e9277be220464af7d3e92
Jose Dzireh Chong <josedzirehchong@gmail.com>
2017-11-24 14:09:56 +1100

    Bot now fully functional, just needs to be pointed correctly

--- academic-biblical-bot/AcademicBiblicalBot.py
+++ academic-biblical-bot/AcademicBiblicalBot.py
@@ -15,16 +15,16 @@ import os
 #TODO
     #Give code a quick read before releasing it
     #Update 40 second age limit of comment to "minimum_comment_age"
-    #Fix distinguishing the comment
     #Change the intervals of time between when this bot is run
     #Find a server to host this bot
     #Check if POSIX time works with DST transitions
+    #Point bot towards AcademicBiblical
     
 #ID of already answered comments goes here
 alreadyRespondedComments = 'PastComments.txt'
 automatedResponse = """This is an automatic notification that your comment has been removed. Your comment was a top level comment, and unless you were hostile it is likely that your comment was removed for not meeting /r/AcademicBiblical's standards. Please check that you have provided academic sources, which are almost always necessary. Once you have edited your comment to comply with the rules, please contact a moderator to get your comment reapproved.
 
-*I am a bot, bleep bloop. Contact my creator /u/JoseDzirehChong if there are any issues with this bot*. [Source code](https://github.com/JoseDzirehChong/academic-biblical-bot)"""
+*I am a bot, bleep bloop. Contact my creator /u/JoseDzirehChong if there are any issues with this bot. [Source code](https://github.com/JoseDzirehChong/academic-biblical-bot)*"""
 
 comment_batch_size = 250
 minimum_comment_age = 1209600
@@ -60,7 +60,7 @@ def check_if_removed(reddit, comment_id):
 
 def run_bot(reddit): #evaluates batches of comments (max batch size is 250 comments)
     
-    print("Getting 250 comments...\n")
+    print("Getting {} comments...\n".format(comment_batch_size))
     
     for comment in reddit.subreddit(subreddit_to_check).comments(limit = comment_batch_size):
         
@@ -68,18 +68,18 @@ def run_bot(reddit): #evaluates batches of comments (max batch size is 250 comme
         creation = comment.created_utc
         age = now - creation
 
-        conditions = comment.parent_id == comment.link_id and find_duplicate_comments(comment.id) == False and age > 40
+        conditions = comment.parent_id == comment.link_id and find_duplicate_comments(comment.id) == False and age < minimum_comment_age
         
         if conditions:
             
             if check_if_removed(reddit, comment.id) == True:
-                print(comment.body)
-                
-                comment.mod.distinguish(sticky=False)
+                print(comment.body + "(removed)")
+                to_distinguish = comment.reply(automatedResponse)
+                to_distinguish.mod.distinguish(sticky=False)
                 save_id(comment)
                 
             elif check_if_removed(reddit, comment.id) == False:
-                print(comment.body)
+                print(comment.body + "(not_removed)")
             
         elif not conditions:
             pass
